bef83ef918c4c6646c523fe6e2d0aec1dcffe02d
Answer1213 <2359305436@qq.com>
2019-11-15 20:54:34 +0800

    第一次合并，没有写入刘搏代码

--- demo-elm/src/Base.js
+++ demo-elm/src/Base.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import P_showcity from './P_showcity'
 import P_denglu from './P_denglu'
+import Start from './Start'
 import {Link,Route,withRouter,Switch,Redirect}  from "react-router-dom"
 export class Base extends Component {
     constructor(props) {
@@ -13,7 +14,8 @@ export class Base extends Component {
         return (
             <div className="App">
                 <section>
-                <Route exact path="/" component={P_showcity} />
+                <Route exact path="/" component={Start} />
+                <Route path="/p_showcity" component={P_showcity} />
                 <Route path="/p_denglu" component={P_denglu} />
                 </section>
             </div>
