fdec5bb4a528b1035513ae857f1efdc4c9de5a98
lanou\lanou <m17739508325@163.com>
2019-11-15 21:12:14 +0800

    第二次合并

--- demo-elm/src/Start.js
+++ demo-elm/src/Start.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
 export class Start extends Component {
     constructor(props) {
       super(props)
-    
       this.state = {
-         
       }
       this.start1=this.start1.bind(this);
-      this.start2=this.start2.bind(this);
     };
     start1(){
-        this.props.history.push({pathname:"P_showcity"});
-        this.props.history.push({pathname:""});
-    }
+        this.props.history.push({pathname:"/P_showcity"});
+    };
     render() {
         return (
             <div>
-                <h1 onClick={start1}>点此进入庞的页面</h1>
-                <h1 onClick={start2}>点此进入刘的页面</h1>
+                <h1 onClick={this.start1}>点此进入庞的页面</h1>
+                <h1>点此进入刘的页面</h1>
             </div>
         )
     }
